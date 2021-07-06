ad69e173492bfb19cbe52aa6995dd5eaba7a145a
xiezihan <zihan.xie@hztl3.com>
2021-07-06 13:43:37 +0800

    react

---
+++ react-preview/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+// import './App.css';
+import HomePage from "./pages/HomePage";
+import UserPage from "./pages/UserPage";
+import ReduxPage from "./pages/ReduxPage";
+
+function App() {
+  return (
+    <div className="App">
+      <ReduxPage />
+    </div>
+  );
+}
+
+export default App;
